ee72cb6b10f4b639baa08b126e817256ff58987c
Jonatas Leon <jonatas.leon@gmail.com>
2016-04-21 15:22:29 -0300

    Token changed again

--- deploy.sh
+++ deploy.sh
@@ -20,11 +20,10 @@ git config user.email "jonatas.leon@gmail.com"
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
 git commit -m "Deploy to Github Pages"
-git status
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 echo "pushing..."
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages # > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
