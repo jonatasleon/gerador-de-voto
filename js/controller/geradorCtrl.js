2d44467f5d6b5934aaf45047bab0685e7123b56a
Jonatas Leon <jonatas.leon@gmail.com>
2016-04-21 15:00:58 -0300

    frases alterado para quem

--- js/controller/geradorCtrl.js
+++ js/controller/geradorCtrl.js
@@ -1,13 +1,15 @@
 angular.module("gerador").controller("GeradorCtrl", ["$scope", "$http",
     function($scope, $http) {
-        var frases = [];
+        var quem = [];
 
         $http.get("frases.json").success(function(data) {
-            Array.prototype.push.apply(frases, data.frases);
+            Array.prototype.push.apply(quem, data.quem);
         });
         $scope.frase = "Clique em \"Gerar\" para saber seu voto";
         $scope.gerarFrase = function() {
-            var frase = frases[Math.floor(Math.random() * frases.length)];
+            var pessoa = quem[Math.floor(Math.random() * quem.length)];
+
+            var frase = "";
             frase += ", eu voto " + ((Math.floor(Math.random() * 2) % 2 === 0) ? "SIM!" : "NÃO!");
 
             $scope.frase = frase;
