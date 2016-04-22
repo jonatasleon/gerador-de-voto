00f92b9805b5825e4bd3f09c8fe0f42cd99c6897
Travis CI <jonatas.leon@gmail.com>
2016-04-22 00:08:21 +0000

    Deploy to Github Pages

---
+++ js/controller/geradorCtrl.js
@@ -0,0 +1,17 @@
+angular.module("gerador").controller("GeradorCtrl", ["$scope", "$http",
+    function($scope, $http) {
+        var quem = [];
+
+        $http.get("frases.json").success(function(data) {
+            Array.prototype.push.apply(quem, data.quem);
+        });
+        $scope.frase = "Clique em \"Gerar\" para saber seu voto";
+        $scope.gerarFrase = function() {
+            var pessoa = quem[Math.floor(Math.random() * quem.length)];
+
+            var frase = pessoa + ", eu voto " + ((Math.floor(Math.random() * 2) % 2 === 0) ? "SIM!" : "NÃO!");
+
+            $scope.frase = frase;
+        };
+    }
+]);
